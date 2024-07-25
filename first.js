558e52246a681bae177614b28a6861a1adc786d1
khandelwalriddhi <ridz1280@gmail.com>
2024-07-25 23:20:39 +0530

    New commit message

---
+++ first.js
@@ -0,0 +1,8 @@
+//1.Swap the values of a and b without using a third variable
+let a=5;
+let b=11;
+console.log("Before Swapping: a="+a+" b="+b);
+a=a+b;
+b=a-b;
+a=a-b
+console.log("After Swapping: a="+a+" b="+b);
---
+++ third.js
@@ -0,0 +1,3 @@
+let num=54;
+let isEven=(num%2==0)?true:false;
+console.log(isEven);
