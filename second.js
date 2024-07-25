48fd8c98774b08e7ed70747572e0aeca85b4dcbf
khandelwalriddhi <ridz1280@gmail.com>
2024-07-25 23:20:39 +0530

    1.Swap the numbers

---
+++ second.js
@@ -0,0 +1,15 @@
+//2.To store maximum of the two elements using if-else
+let x=55;
+let y=21;
+let max;
+if(x>y){
+    max=x;
+}
+else{
+    max=y;
+}
+console.log("Maximum of the two is "+max);
+
+//using ternary operator
+let maxi=(x>y)?x:y;
+console.log("Maximum of the two is "+maxi);
